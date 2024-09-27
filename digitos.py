e9b48ba3eac10fc564ed5f35e92faed19f61d763
alexander gomez <alexanderps082@gmail.com>
2024-09-27 18:30:52 -0500

    Agregar digitos.py

---
+++ digitos.py
@@ -0,0 +1,6 @@
+empieza = int(input("escriba desde donde contar: "))
+cantidad = int(input("escriba hasta donde contar: "))
+
+for i in range(empieza,cantidad + 1):
+    print(f"los numeros son: {i}")
+    
